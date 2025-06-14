ed438a9638cef17947b4464aa7ee80e2cb0f5005
Praneeth Tom <praneeth.tom@gmail.com>
2025-06-14 14:08:06 +0000

    let the container of the breathing animation be the same but increase th

--- src/components/BreathingCircle.tsx
+++ src/components/BreathingCircle.tsx
@@ -5,9 +5,9 @@ import { cn } from '@/lib/utils';
 
 const BreathingCircle = () => {
   // Keyframes for 'breathing' are defined in globals.css
-  // Tailwind's JIT compiler will pick up 'animate-[breathing_...]'
+  // This component's root div will now fill the parent container from CurrentlyPlayingCard.
   return (
-    <div className="relative flex items-center justify-center w-20 h-20">
+    <div className="relative flex items-center justify-center w-full h-full">
       {/* Circle 1 (visually back, longest delay, appears to be "behind") */}
       <div
         className={cn(
@@ -27,7 +27,6 @@ const BreathingCircle = () => {
         className={cn(
           "absolute w-full h-full rounded-full bg-primary animate-[breathing_2s_ease-out_infinite_normal]"
         )}
-        // No style={{ animationDelay: '0s' }} needed as 0s is the default behavior for the animation property
       />
     </div>
   );
